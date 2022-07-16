3a365da61b2f54b72fe382da941e16caee86d81e
Parth Pitrubhakta <parthwani1990@gmail.com>
2022-07-16 16:14:33 +0530

    Changes in the code of Home fragment

--- app/src/main/java/com/example/findaim/HomeFragment.kt
+++ app/src/main/java/com/example/findaim/HomeFragment.kt
@@ -12,81 +12,50 @@ import com.bumptech.glide.Glide
 import com.example.findaim.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
-    private lateinit var bindingHomeBinding: FragmentHomeBinding
+    private var binding : FragmentHomeBinding? = null
     private lateinit var firebaseAuth: FirebaseAuth
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        val fragmentBinding = FragmentHomeBinding.inflate(inflater,  container,false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         firebaseAuth = FirebaseAuth.getInstance()
         val currentUser = firebaseAuth.currentUser
 
-        bindingHomeBinding.tvNameMainpg.text = currentUser?.displayName
+        binding!!.tvNameMainpg.text = currentUser?.displayName
 
-        Glide.with(this).load(currentUser?.photoUrl).into(bindingHomeBinding.IbHomepageprofilephoto)
+        Glide.with(this).load(currentUser?.photoUrl).into(binding!!.IbHomepageprofilephoto)
 
-        bindingHomeBinding.IbHomepageprofilephoto.setOnClickListener{
+        binding!!.IbHomepageprofilephoto.setOnClickListener{
             findNavController().navigate(R.id.profileFragment)
         }
 
-        bindingHomeBinding.btnGotowebsite.setOnClickListener {
-
+        binding!!.btnGotowebsite.setOnClickListener {
             val intent= Intent(Intent.ACTION_VIEW, Uri.parse("https://find-aim.com//"))
             startActivity(intent)
         }
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View{
-        bindingHomeBinding = FragmentHomeBinding.inflate(inflater,container,false)
-        // Inflate the layout for this fragment
-        return bindingHomeBinding.root
+    fun openLink(link: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(intent)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
 }
