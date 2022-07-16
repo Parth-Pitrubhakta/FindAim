e50278dbcb8bfeccd9721f30f5d13d9d02bbfb7e
Parth Pitrubhakta <parthwani1990@gmail.com>
2022-07-16 20:26:00 +0530

    Scroll view implemented

--- app/src/main/java/com/example/findaim/MainActivity.kt
+++ app/src/main/java/com/example/findaim/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.findaim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.findaim.databinding.ActivityMainBinding
-import com.example.findaim.ui.HomeFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +22,10 @@ class MainActivity : AppCompatActivity() {
         //Initialize the bottom navigation view
         //create bottom navigation view object
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_fragment) as NavHostFragment
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        binding.bottomNavigatinView.setupWithNavController(navController)
-        binding.bottomNavigatinView.itemIconTintList
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.itemIconTintList
 
 //        val bottomNavigationView = findViewById<BottomNavigationView
 //                >(R.id.bottom_navigatin_view)
