c5307fc0c9401f8119a9d4e5f9105114bfa56077
Parth Pitrubhakta <parthwani1990@gmail.com>
2022-07-17 09:08:57 +0530

    Implemented recycler view animations

--- app/src/main/java/com/example/findaim/adapter/ImagesAdapter.kt
+++ app/src/main/java/com/example/findaim/adapter/ImagesAdapter.kt
@@ -3,17 +3,21 @@ package com.example.findaim.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.findaim.R
 import com.example.findaim.R.id.itemImage
+import com.example.findaim.R.id.itemImages_cardview
 import com.example.findaim.data.Imagedata
 
 
 class ImagesAdapter(var Images_data: List<Imagedata>) : RecyclerView.Adapter<ImagesAdapter.IntViewHolder>() {
     class IntViewHolder(row: View) : RecyclerView.ViewHolder(row) {
         val imageView = row.findViewById<ImageView>(itemImage)
+        val cardView = row.findViewById<CardView>(itemImages_cardview)
 
     }
 
@@ -28,6 +32,7 @@ class ImagesAdapter(var Images_data: List<Imagedata>) : RecyclerView.Adapter<Ima
     }
 
     override fun onBindViewHolder(holder: IntViewHolder, position: Int) {
+        holder.cardView.animation = AnimationUtils.loadAnimation(holder.itemView.context,R.anim.example_anims)
         val item=Images_data[position]
         holder.imageView.setBackgroundResource(item.img)
     }
